ea69e2a3a3481ebb50a47e17901e3079d4697711
muhammed sibly <tech.sibly@gmail.com>
2022-11-19 10:33:11 +0530

    error 22, mongo cluster

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const state={
 }
 
 module.exports.connect=function(done){
-    const url='mongodb://localhost:27017';
+    const url='mongodb+srv://sibly26462:q3ttvYcVEpp70v1K@cluster0.4bfqwq7.mongodb.net/?retryWrites=true&w=majority'; 
     const dbname='DressMart';
     MongoClient.connect(url,function (err,data){
         if(err) return done(err);
